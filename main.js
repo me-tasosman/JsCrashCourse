d1081e8ab49fc38945700e22f41c9fbaa3bdd223
Osman Tas <me@tasosman.com>
2020-12-09 22:24:28 +0300

    first

---
+++ main.js
@@ -0,0 +1,28 @@
+/*
+console.log("Hello World");
+console.error("Error");
+console.warn("This is warning");
+*/
+
+// let, const
+
+let agea = 40;
+agea = 41;
+console.log(agea);
+
+// data typees
+// String numbers Boolean null undefined
+
+const name = "John";
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+
+// concatenation
+
+console.log("My name is " + name + " and I am " + age);
+
+// Template String
+console.log(`My name is ${name} and I am ${age}`);
